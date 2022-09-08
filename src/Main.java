8970a382c2a4dd7758c1ab8dace8a52cf6ab957b
okhremenko59 <okhremenko@bk.ru>
2022-09-08 19:14:54 +0500

    add array sale

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int countProduct = 0;
         Scanner scanner = new Scanner(System.in);
         Product[] product = {new Product("Молоко", 50),
                 new Product("Хлеб", 14),
                 new Product("Каша гречневая", 80)};
+
 
         System.out.println("Список доступных продуктов к покупке: ");
         for (int i = 0; i < product.length; i++) {
@@ -14,6 +16,19 @@ public class Main {
             System.out.println(cnt + ". " + product[i]);
         }
 
+        Product[] productSale = {new Product("Кефир", 50),
+                new Product("Макароны", 100),
+                new Product("Творог", 200)};
+
+
+        System.out.println("Список доступных продуктов к покупке по акции 3 по цене 2-х: ");
+
+        for (int i = 0; i < productSale.length; i++) {
+            int cnt = product.length+i;
+            System.out.println(cnt+1 + ". " + productSale[i]);
+        }
+
+        countProduct=product.length+productSale.length;
 
         while (true) {
 
@@ -37,7 +52,7 @@ public class Main {
                 } else
                     System.out.println("вы сделали ввод не из двух частей, а из одной или более двух");
 
-                if ((((productNumber + 1) > product.length)) || ((productNumber + 1) < 1)) {
+                if (((((productNumber + 1)) > countProduct)) || ((productNumber + 1) < 1)) {
                     System.out.println("нет такого продукта");
                 }
 
@@ -48,6 +63,9 @@ public class Main {
             try {
                 product[productNumber].sum += product[productNumber].price * productCount;
                 product[productNumber].productName = product[productNumber].productName;
+
+                productSale[productNumber].sum += productSale[productNumber].price * productCount;
+                productSale[productNumber].productName = productSale[productNumber].productName;
             } catch (ArrayIndexOutOfBoundsException c) {
             }
         }
